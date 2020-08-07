08c613ee27f80f85418de9e932dcca2198933720
cchura94 <cchura.cpc@gmail.com>
2020-08-06 20:41:56 -0400

    crud de productos

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const Usuario = require("./../models/usuario");
 const usuarioController = require("../controllers/usuarioController");
 const clienteController = require("../controllers/clienteController");
+const productoController = require("../controllers/productoController");
 
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middlewares/auth");
@@ -31,4 +32,11 @@ module.exports.add = (app) => {
   app.get("/cliente/:id", authMiddleware.verificaAuth, clienteController.mostrar);
   app.put("/cliente/:id", authMiddleware.verificaAuth, clienteController.modificar);
   app.delete("/cliente/:id", authMiddleware.verificaAuth, clienteController.eliminar);
+
+  // Rutas de Productos.
+  app.get("/producto", authMiddleware.verificaAuth, productoController.listar);
+  app.post("/producto", authMiddleware.verificaAuth, productoController.guardar);
+  app.get("/producto/:id", authMiddleware.verificaAuth, productoController.mostrar);
+  app.put("/producto/:id", authMiddleware.verificaAuth, productoController.modificar);
+  app.delete("/producto/:id", authMiddleware.verificaAuth, productoController.eliminar);
 };
